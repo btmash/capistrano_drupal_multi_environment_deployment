b85e4452fdb54ad69006ecf9deabea1afc3fe410
Ashok Modi <btmash@gmail.com>
2013-03-12 01:02:50 -0700

    Add stub aliases file.

---
+++ aliases.drushrc.php
@@ -0,0 +1,32 @@
+<?php
+
+$aliases['dev'] = array(
+	'remote-host' => 'dev.domain.com',
+	'remote-user' => 'username',
+	'root' => '/path/to/application',
+	'uri' => 'default',
+	'path-aliases' => array(
+		'%private_files' => '/path/to/private_directory',
+	),
+);
+
+$aliases['staging'] = array(
+	'remote-host' => 'staging.domain.com',
+	'remote-user' => 'username',
+	'root' => '/path/to/application',
+	'uri' => 'default',
+	'path-aliases' => array(
+		'%private_files' => '/path/to/private_directory',
+	),
+);
+
+$aliases['production'] = array(
+	'remote-host' => 'production.domain.com',
+	'remote-user' => 'username',
+	'root' => '/path/to/application',
+	'uri' => 'default',
+	'path-aliases' => array(
+		'%private_files' => '/path/to/private_directory',
+	),
+);
+
